f8f79ecb5c1dc17ab8f7179a813b14cc5c772a93
Паньшин Евгений <e.panshin@ylab.team>
2024-02-27 16:11:31 +0300

    Каркас модуля, шаблон миграций, композер инит файлы

---
+++ local/php_interface/init.php
@@ -0,0 +1,11 @@
+<?php
+
+if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+use Bitrix\Main\Loader;
+
+# composer autoload
+
+# load modules:
---
+++ local/php_interface/migrations.cfg.php
@@ -0,0 +1,5 @@
+<?php return array (
+    'version_name_template' => 'M#TIMESTAMP#_#NAME#',
+    'migration_dir' => '/local/php_interface/migrations',
+    'migration_table' => 'sprint_migration_versions',
+);
